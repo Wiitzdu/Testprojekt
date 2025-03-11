890bd3928e4a269cd1c093e0b9f0ddd0c6d5c22a
Eddienator <eddie.richter98@gmail.com>
2025-03-11 16:15:25 +0100

    teleportation

---
+++ Universe of Materials/Assets/Scripts/TriggerEnter.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEnter : MonoBehaviour
+{
+    public UnityEvent enteredTrigger;
+    public UnityEvent exitTrigger;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            enteredTrigger?.Invoke();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            exitTrigger?.Invoke();
+        }
+    }
+}
