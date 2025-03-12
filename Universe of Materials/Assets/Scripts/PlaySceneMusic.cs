08becd71ad9ec4e6a79431387df367a8a7c2cbf0
Wiitzdu <wiitzdu@web.de>
2025-03-12 21:41:02 +0100

    main music and transition sound prerequisites

---
+++ Universe of Materials/Assets/Scripts/PlaySceneMusic.cs
@@ -0,0 +1,38 @@
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayPadLoop : MonoBehaviour
+{
+    [SerializeField] EventReference PadEvent;
+    private EventInstance padInstance;
+
+    //void Start()
+    //{
+    //    padInstance = RuntimeManager.CreateInstance(PadEvent);
+    //    padInstance.start();
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    if (Keyboard.current.spaceKey.wasPressedThisFrame) // Change 'spaceKey' to any key
+    //    {
+    //        padInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+    //        padInstance.release(); // Clean up the instance
+    //    }
+    //}
+
+    private void Awake()
+    {
+        padInstance = RuntimeManager.CreateInstance(PadEvent);
+        padInstance.start();
+    }
+
+    private void OnDestroy()
+    {
+        padInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        padInstance.release(); // Clean up the instance
+    }
+}
